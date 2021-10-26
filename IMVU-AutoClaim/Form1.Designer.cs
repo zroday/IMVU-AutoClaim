75aa9ceb1d867686eb4fa6b6a72b96266360b831
zroday <eu.zeroday@gmail.com>
2021-10-26 19:27:05 -0300

    Adicionar arquivos de projeto.

---
+++ IMVU-AutoClaim/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace IMVU_AutoClaim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(177, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(78, -15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Credits?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(28, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Start!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(203, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Profile?";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 22);
+            this.panel1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(174, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "User:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(12, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Your credits:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 210);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(281, 22);
+            this.panel2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(12, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "status:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(121, 5);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Loading...";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Status:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(56, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "not started";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(161, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "User:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(161, 83);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Password:";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3600000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1, 194);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(227, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "https://github.com/zroday/IMVU-AutoClaim.git";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(281, 232);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "IMVU - AutoClaim";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ IMVU-AutoClaim/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace IMVU_AutoClaim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Button.CheckForIllegalCrossThreadCalls = false;
+
+            //webBrowser1.Navigate("https://imvu.com/next/home/", null, null, "User-Agent:Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36");
+        }
+
+        //create cefBrowser instance
+        public ChromiumWebBrowser browser;
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            browser = new ChromiumWebBrowser("https://pt.secure.imvu.com/");
+            browser.Dock = DockStyle.Fill;
+            
+            Controls.Add(browser);
+            browser.Visible = false;
+            browser.LoadingStateChanged += OnLoadingStateChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //chromiumWebBrowser1.Load("https://www.google.com/");
+            label6.Text = "Please login to get started";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Click login button
+            var scriptClickLoginPage = @"document.getElementsByClassName('sign-in')[0].click();";
+            browser.ExecuteScriptAsync(scriptClickLoginPage);
+
+            //Fill in the access fields
+            var scriptFillUserName = @"document.getElementsByName('avatarname')[0].value = '" + textBox1.Text + "'";
+            var scriptFillPassword = @"document.getElementsByName('password')[0].value = '" + textBox2.Text + "'";
+
+            browser.ExecuteScriptAsync(scriptFillUserName);
+            browser.ExecuteScriptAsync(scriptFillPassword);
+
+            //Click login button
+            var scriptClickLoginButton = @"document.getElementsByClassName('btn-primary')[0].click();";
+            browser.ExecuteScriptAsync(scriptClickLoginButton);
+
+
+            //check if the user page was loaded correctly
+            while (checkLogin() == false)
+            {
+                checkLogin();
+                
+            }
+
+            button1.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+            timer1.Start();
+        }
+
+        public bool checkLogin()
+        {
+            if (browser.Address.ToString().Contains("/next/home/"))
+            {
+                MessageBox.Show("Success, your account " + "(" + textBox1.Text + ")" + " has been logged in");
+                label2.Text = textBox1.Text;
+                label6.Text = "Loading...";
+                return true;
+                
+            }
+
+            return false;
+        }
+       
+        //document.getElementsByClassName('reward-name')[0].textContent;
+        //document.getElementsByClassName('reward-description')[0].textContent;
+        //document.getElementsByClassName('dialog-x')[0].click();
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            //Get Credit balance
+            string scriptChkCredits = "document.getElementsByClassName('wallet-balance__value')[0].textContent;";
+            JavascriptResponse response = await browser.EvaluateScriptAsync(scriptChkCredits);
+            string resultS = response.Result.ToString();
+
+            label1.Text = resultS;
+
+            label6.Text = "AutoClaim started successfully!";
+            label8.Text = "Working!";
+            label8.ForeColor = Color.Green;
+            timer2.Start();
+        }
+
+
+        //Check that the web page has completely loaded
+        //Invoke button login
+        private void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
+        {
+            if (!args.IsLoading)
+            {
+                button1.BeginInvoke((Action)delegate ()
+                {
+                    button1.Enabled = true;
+                    
+                });
+            }
+        }
+
+        //Start page load timer1
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            label6.Text = "Please start now";
+            timer1.Stop();
+        }
+
+        //Start autoClaim injection
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //Click wallet button
+            var scriptClickWallet = @"document.getElementsByClassName('wallet-balance')[0].click();";
+            browser.ExecuteScriptAsync(scriptClickWallet);
+            //check if the bonus is released
+            var scriptCheckBonus = @"document.getElementsByClassName('available')[0].click();";
+            browser.ExecuteScriptAsync(scriptCheckBonus);
+            //spin now
+            var scriptSpinNow = @"document.getElementsByClassName('spin')[0].click();";
+            browser.ExecuteScriptAsync(scriptSpinNow);
+            //Close Spin
+            var scriptCloseSpin = @"document.getElementsByClassName('dialog-x')[0].click();";
+            browser.ExecuteScriptAsync(scriptCloseSpin);
+        }
+    }
+}
